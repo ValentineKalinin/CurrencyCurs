f25a9a3f6f2939b775926f7e38617e82b9623a3f
”Valentine <kalvalkaltan@gmail.com>
2023-02-13 02:19:00 +0300

    Added saving the query result to .json file

--- src/main/java/com/nces/demo/Controller.java
+++ src/main/java/com/nces/demo/Controller.java
@@ -1,6 +1,6 @@
 package com.nces.demo;
 
-import org.jfree.data.general.Dataset;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -10,23 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.swing.JFrame;
+import java.io.*;
 import java.util.*;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
 public class Controller {
     @Value("${allCurrencyUrl}")
     private String allCurrencyUrl;
-
     @Value("${curIdUrl}")
     private String curIdUrl;
     RestTemplateBuilder restTemplateBuilder;
@@ -36,45 +28,43 @@ public class Controller {
         this.restTemplateBuilder = restTemplateBuilder;
     }
 
-
     // Получить список всех валют - http://localhost:8080/test/currencies
     @GetMapping(value = "/currencies", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Results> GetAllCurrency() {
         RestTemplate restTemplate = restTemplateBuilder.build();
         ResponseEntity<Results[]> response = restTemplate.getForEntity(allCurrencyUrl, Results[].class);
         Results[] cur = response.getBody();
+        // System.out.println("all currencues here");
         assert cur != null;
         return Arrays.stream(cur).toList();
     }
 
-
-    // Получить валюту по айди за период - http://localhost:8080/test/history?cur_id=290&startdate=2001-12-30&enddate=2050-01-01
+    // Получить валюту за период - http://localhost:8080/test/history?cur_id=290&startdate=2001-12-30&enddate=2050-01-01
     @GetMapping(value = "/history", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<RateShort> GetHistoryCurrency(
             @RequestParam(value = "cur_id") int cur_id,
-            @RequestParam(value = "startdate") @DateTimeFormat(pattern="dd.MM.yyyy") String startdate,
-            @RequestParam(value = "enddate") @DateTimeFormat(pattern="dd.MM.yyyy") String enddate) {
-        Map<String, String> uriParams = new HashMap<String, String>();
+            @RequestParam(value = "startdate") @DateTimeFormat(pattern = "dd.MM.yyyy") String startdate,
+            @RequestParam(value = "enddate") @DateTimeFormat(pattern = "dd.MM.yyyy") String enddate) throws IOException {
+        Map<String, String> uriParams = new HashMap<>();
+        // System.out.println("request done");
         uriParams.put("cur_id", String.valueOf(cur_id));
         uriParams.put("startdate", startdate);
         uriParams.put("enddate", enddate);
         RestTemplate restTemplate = restTemplateBuilder.build();
         ResponseEntity<RateShort[]> response = restTemplate.getForEntity(curIdUrl, RateShort[].class, uriParams);
         RateShort[] cur = response.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        File myFile = new File("src/main/resources/templates/historyOfCurrency.json");
+        mapper.writeValue(myFile, cur);
+        // System.out.println("file created");
         assert cur != null;
         //DrawPlot();
         return Arrays.stream(cur).toList();
     }
 
+    // Нарисовать график валюты за период --
     public void DrawPlot() {
-        XYDataset dataset = new XYSeriesCollection();
-        JFreeChart chart = ChartFactory.createXYLineChart("", "", "", dataset);
-        ChartPanel chartPanel = new ChartPanel(chart);
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(chartPanel);
-        frame.pack();
-        frame.setVisible(true);
+
     }
 
 
