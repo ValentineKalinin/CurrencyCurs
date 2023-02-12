5e43cae02afff34e8dce3d6005b0a0f3b0fa80f0
”Valentine <kalvalkaltan@gmail.com>
2023-02-13 02:20:37 +0300

    Unused files

--- src/main/java/com/nces/demo/CurrencyAbbreviation.java
+++
@@ -1,8 +0,0 @@
-package com.nces.demo;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class CurrencyAbbreviation {
-    @JsonProperty(value = "Cur_Abbreviation")
-    String currencyAbbreviation;
-}
--- src/main/java/com/nces/demo/CurrencyId.java
+++
@@ -1,8 +0,0 @@
-package com.nces.demo;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class CurrencyId {
-    @JsonProperty(value = "Cur_Id")
-    String currencyID;
-}
