9ccc04a8e612b721398406a0c626cad8c8726910
”Valentine <kalvalkaltan@gmail.com>
2023-02-13 02:17:26 +0300

    Delete the unnecessary field from the received .json file

--- src/main/java/com/nces/demo/RateShort.java
+++ src/main/java/com/nces/demo/RateShort.java
@@ -1,17 +1,17 @@
 package com.nces.demo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class RateShort {
-    @JsonProperty(value = "Cur_ID")
-    String currencyID;
     @JsonProperty(value = "Date")
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     String currencyDate;
     @JsonProperty(value = "Cur_OfficialRate")
+    @DateTimeFormat(pattern = "dd.MM.yyyy")
     String currencyRate;
 
-    public RateShort(String currencyID, String currencyDate, String currencyRate) {
-        this.currencyID = currencyID;
+    public RateShort(String currencyDate, String currencyRate) {
         this.currencyDate = currencyDate;
         this.currencyRate = currencyRate;
     }
