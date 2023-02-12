6918d5f66e5b84a4904cf7367998d0b107c26e63
”Valentine <kalvalkaltan@gmail.com>
2023-02-13 02:12:25 +0300

    Made the browser open automatically when the application is launched

--- src/main/java/com/nces/demo/TestApplication.java
+++ src/main/java/com/nces/demo/TestApplication.java
@@ -3,11 +3,35 @@ package com.nces.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
 public class TestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+        browse("http://localhost:63342/demo/static/index.html");
+        System.out.println("app started");
+    }
 
+    public static void browse(String url) {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
