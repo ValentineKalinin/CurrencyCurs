925e06d24339d8a4a098f65b15cae1c564183ced
”Valentine <kalvalkaltan@gmail.com>
2023-02-17 01:45:43 +0300

    Interface for automatically opening the browser

---
+++ src/main/java/com/nces/demo/browse.java
@@ -0,0 +1,26 @@
+package com.nces.demo;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public interface browse {
+    static void browser(String url) {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
