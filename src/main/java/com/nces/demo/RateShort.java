05e66742463e34b33b785cb10fa839d70a57e630
”Valentine <kalvalkaltan@gmail.com>
2023-02-17 01:38:39 +0300

    Classes for received data after requests

--- src/main/java/com/nces/demo/RateShort.java
+++ src/main/java/com/nces/demo/RateShort.java
@@ -3,16 +3,59 @@ package com.nces.demo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
 public class RateShort {
     @JsonProperty(value = "Date")
     @DateTimeFormat(pattern = "dd.MM.yyyy")
-    String currencyDate;
+    private Date currencyDate;
     @JsonProperty(value = "Cur_OfficialRate")
-    @DateTimeFormat(pattern = "dd.MM.yyyy")
-    String currencyRate;
+    private Double currencyRate;
 
-    public RateShort(String currencyDate, String currencyRate) {
+    public RateShort() {}
+
+    public RateShort(Date currencyDate, Double currencyRate) {
         this.currencyDate = currencyDate;
         this.currencyRate = currencyRate;
+    }
+
+    public Date getCurrencyDate() {
+        return currencyDate;
+    }
+
+    public void setCurrencyDate(Date currencyDate) {
+        this.currencyDate = currencyDate;
+    }
+
+    public Double getCurrencyRate() {
+        return currencyRate;
+    }
+
+    public void setCurrencyRate(Double currencyRate) {
+        this.currencyRate = currencyRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RateShort rateShort)) return false;
+
+        if (!getCurrencyDate().equals(rateShort.getCurrencyDate())) return false;
+        return getCurrencyRate().equals(rateShort.getCurrencyRate());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getCurrencyDate().hashCode();
+        result = 31 * result + getCurrencyRate().hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "RateShort{" +
+                "currencyDate=" + currencyDate +
+                ", currencyRate='" + currencyRate + '\'' +
+                '}';
     }
 }
--- src/main/java/com/nces/demo/Results.java
+++ src/main/java/com/nces/demo/Results.java
@@ -3,6 +3,7 @@ package com.nces.demo;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Results implements Serializable {
     @JsonProperty(value = "Cur_ID")
@@ -19,5 +20,59 @@ public class Results implements Serializable {
         this.currencyAbbreviation = currencyAbbreviation;
         this.currencyStartDate = currencyStartDate;
         this.currencyEndDate = currencyEndDate;
+    }
+
+    public String getCurrencyID() {
+        return currencyID;
+    }
+
+    public void setCurrencyID(String currencyID) {
+        this.currencyID = currencyID;
+    }
+
+    public String getCurrencyAbbreviation() {
+        return currencyAbbreviation;
+    }
+
+    public void setCurrencyAbbreviation(String currencyAbbreviation) {
+        this.currencyAbbreviation = currencyAbbreviation;
+    }
+
+    public String getCurrencyStartDate() {
+        return currencyStartDate;
+    }
+
+    public void setCurrencyStartDate(String currencyStartDate) {
+        this.currencyStartDate = currencyStartDate;
+    }
+
+    public String getCurrencyEndDate() {
+        return currencyEndDate;
+    }
+
+    public void setCurrencyEndDate(String currencyEndDate) {
+        this.currencyEndDate = currencyEndDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Results results)) return false;
+        return getCurrencyID().equals(results.getCurrencyID()) && getCurrencyAbbreviation().equals(results.getCurrencyAbbreviation()) && getCurrencyStartDate().equals(results.getCurrencyStartDate()) && getCurrencyEndDate().equals(results.getCurrencyEndDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCurrencyID(), getCurrencyAbbreviation(), getCurrencyStartDate(), getCurrencyEndDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Results{" +
+                "currencyID='" + currencyID + '\'' +
+                ", currencyAbbreviation='" + currencyAbbreviation + '\'' +
+                ", currencyStartDate='" + currencyStartDate + '\'' +
+                ", currencyEndDate='" + currencyEndDate + '\'' +
+                '}';
     }
 }
